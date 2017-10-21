948dd4c97344e050e5fd4ea22e65713d8e54607c
ryanlb22 <rlb.lebon@gmail.com>
2017-10-21 00:43:13 +0000

    E

--- simple-function.js
+++ simple-function.js
@@ -2,5 +2,9 @@ function add(x, y) {
     var sum = x + y; 
     return sum; 
 }
-
+function multiply(a,b){
+    var m = a * b;
+    return m;
+}
 module.exports.add = add; 
+module.exports.multiply = multiply;
